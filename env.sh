4e50b11afc78292a47c4c551b3ee1342236718e4
Fakhri <41115223+fskhri@users.noreply.github.com>
2023-06-27 17:28:14 +0700

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,23 +1,23 @@
 #! /bin/bash
 
 # Kernel Repo
-KERNEL_REPO=https://"${GITHUB_USER}":"${GITHUB_TOKEN}"@github.com/fskhri/android_kernel_realme_RM6785
+KERNEL_REPO=https://"${GITHUB_USER}":"${GITHUB_TOKEN}"@github.com/fskhri/YorForger_Plato_Xiaomi_Kernel
 
 # Kernel Branch
-KERNEL_BRANCH=master
+KERNEL_BRANCH=bsp-plato-s-oss
 
 # The name of the device for which the kernel is built
-MODEL="Realme 7,6,6i, 6s, Narzo 20 Pro and Narzo 30"
+MODEL="XIAOMI 12T"
 
 # The codename of the device
-DEVICE="MT6785"
+DEVICE="plato"
 
 # The defconfig which should be used. Get it from config.gz from
 # your device or check source
-DEFCONFIG=RM6785_defconfig
+DEFCONFIG=gki_defconfig
 
 # Show manufacturer info
-MANUFACTURERINFO="Realme"
+MANUFACTURERINFO="XIAOMI"
 
 # tambahkan changelog di anykernel
 CHANGELOGS=n
@@ -40,7 +40,7 @@ BUILD_TYPE="OFFICIAL"
 
 # Specify compiler.
 # 'clang' or 'clangxgcc' or 'gcc' or 'gcc49' , 'linaro & 'gcc2', clang2
-COMPILER=clang2
+COMPILER=clang
 
 # Message on anykernel when installatio
 MESSAGE="just flash and forget"
